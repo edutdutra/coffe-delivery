82afe24f75f7e9c5ec8af8ab705d7b5dcbea85f4
edutdutra <edutdutra@gmail.com>
2023-07-25 09:09:55 -0300

    refactor: add new component to home banner

---
+++ src/pages/Home/Banner/index.tsx
@@ -0,0 +1,59 @@
+import {
+    BannerContainer,
+    BannerContent,
+    BannerItem,
+    BannerItemsColumn,
+    BannerItemsContainer, IconCircle,
+    Subtitle,
+    Title
+} from "./styles.ts";
+
+import {Coffee, Package, ShoppingCart, Timer} from "@phosphor-icons/react";
+import coffeeHome from "../../../assets/coffee-home.svg";
+
+export function Banner() {
+    return (
+        <BannerContainer>
+            <BannerContent>
+                <Title>Encontre o café perfeito para qualquer hora do dia</Title>
+                <Subtitle>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</Subtitle>
+
+                <BannerItemsContainer>
+                    <BannerItemsColumn>
+                        <BannerItem>
+                            <IconCircle circleColor="yellowDark">
+                                <ShoppingCart size={16} weight="fill"/>
+                            </IconCircle>
+                            <span>Compra simples e segura</span>
+                        </BannerItem>
+
+                        <BannerItem>
+                            <IconCircle circleColor="yellow">
+                                <Timer size={16} weight="fill"/>
+                            </IconCircle>
+                            <span>Entrega rápida e rastreada</span>
+                        </BannerItem>
+
+                    </BannerItemsColumn>
+                    <BannerItemsColumn>
+                        <BannerItem>
+                            <IconCircle circleColor="gray">
+                                <Package size={16} weight="fill"/>
+                            </IconCircle>
+                            <span>Embalagem mantém o café intacto</span>
+                        </BannerItem>
+
+                        <BannerItem>
+                            <IconCircle circleColor="purple">
+                                <Coffee size={16} weight="fill"/>
+                            </IconCircle>
+                            <span>O café chega fresquinho até você</span>
+                        </BannerItem>
+                    </BannerItemsColumn>
+                </BannerItemsContainer>
+            </BannerContent>
+
+            <img src={coffeeHome} alt=""/>
+        </BannerContainer>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,62 +1,12 @@
-import {
-    BannerContainer,
-    BannerContent,
-    BannerItem,
-    BannerItemsColumn,
-    BannerItemsContainer,
-    HomeContainer, IconCircle,
-    Subtitle,
-    Title
-} from "./styles.ts";
-import coffeHome from "../../assets/coffe-home.svg";
-import {Coffee, Package, ShoppingCart, Timer} from "@phosphor-icons/react";
+import {Banner} from "./Banner";
+
+import {HomeContainer} from "./styles.ts";
+
 
 export function Home() {
     return (
         <HomeContainer>
-            <BannerContainer>
-                <BannerContent>
-                    <Title>Encontre o café perfeito para qualquer hora do dia</Title>
-                    <Subtitle>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</Subtitle>
-
-                    <BannerItemsContainer>
-                        <BannerItemsColumn>
-                            <BannerItem>
-                                <IconCircle circleColor="yellowDark">
-                                    <ShoppingCart size={16} weight="fill"/>
-                                </IconCircle>
-                                <span>Compra simples e segura</span>
-                            </BannerItem>
-
-                            <BannerItem>
-                                <IconCircle circleColor="yellow">
-                                    <Timer size={16} weight="fill"/>
-                                </IconCircle>
-                                <span>Entrega rápida e rastreada</span>
-                            </BannerItem>
-
-                        </BannerItemsColumn>
-                        <BannerItemsColumn>
-                            <BannerItem>
-                                <IconCircle circleColor="gray">
-                                    <Package size={16} weight="fill"/>
-                                </IconCircle>
-                                <span>Embalagem mantém o café intacto</span>
-                            </BannerItem>
-
-                            <BannerItem>
-                                <IconCircle circleColor="purple">
-                                    <Coffee size={16} weight="fill"/>
-                                </IconCircle>
-                                <span>O café chega fresquinho até você</span>
-                            </BannerItem>
-                        </BannerItemsColumn>
-                    </BannerItemsContainer>
-                </BannerContent>
-
-                <img src={coffeHome} alt=""/>
-            </BannerContainer>
-
+            <Banner />
 
             <span>LISTA DE CAFÉS</span>
         </HomeContainer>
